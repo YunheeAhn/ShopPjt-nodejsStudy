bb8e0cb39cf40519278aa377ccfead33616870ba
Yunning <ayh95944674@gmail.com>
2026-02-20 23:15:52 +0900

    feat : 벡엔드 상품 생성, 가져오기 기능 추가

--- SHOPPJT-BE/controller/product.controller.js
+++ SHOPPJT-BE/controller/product.controller.js
@@ -5,7 +5,7 @@ const Product = require("../models/Product");
 
 const productController = {};
 
-// 상품 정보 가져오기
+// 상품 정보 생성하기
 productController.createProduct = async (req, res) => {
   try {
     const { sku, name, size, image, category, description, price, stock, status } = req.body;
@@ -23,6 +23,16 @@ productController.createProduct = async (req, res) => {
 
     await product.save();
     res.status(200).json({ status: "success", product });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+// 상품 정보 가져오기
+productController.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ status: "success", products });
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
--- SHOPPJT-BE/routes/product.api.js
+++ SHOPPJT-BE/routes/product.api.js
@@ -1,4 +1,4 @@
-//따로 분리 된 /auth 라우터 파일
+//따로 분리 된 /product 라우터 파일
 
 // express 모듈 불러오기
 const express = require("express");
@@ -7,12 +7,16 @@ const router = express.Router();
 const authController = require("../controller/auth.controller");
 const productController = require("../controller/product.controller");
 
+// 상품 생성 endpoint 연결
 router.post(
   "/",
   authController.authenticate,
   authController.checkAdminPermission,
   productController.createProduct,
 );
+
+// 상품 가져오기 endpoint 연결
+router.get("/", productController.getProducts);
 
 // 라우터 모듈 내보내기
 module.exports = router;
