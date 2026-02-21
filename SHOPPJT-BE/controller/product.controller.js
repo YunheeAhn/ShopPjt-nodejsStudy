ba2edde5f1db5b6c827134b918cf18b65a97e283
Yunning <ayh95944674@gmail.com>
2026-02-21 23:34:28 +0900

    feat : 벡엔드 상품 검색, 페이지네이션 추가

--- SHOPPJT-BE/controller/product.controller.js
+++ SHOPPJT-BE/controller/product.controller.js
@@ -3,6 +3,8 @@
 // model 불러오기
 const Product = require("../models/Product");
 
+const PAGE_SIZE = 5;
+
 const productController = {};
 
 // 상품 정보 생성하기
@@ -36,8 +38,25 @@ productController.createProduct = async (req, res) => {
 // 상품 정보 가져오기
 productController.getProducts = async (req, res) => {
   try {
-    const products = await Product.find({});
-    res.status(200).json({ status: "success", products });
+    // 쿼리 값 읽어오기
+    const { page, name } = req.query;
+
+    // 검색조건 선언
+    // regex : query를 포함한 name까지, option-i : 대소문자 구분X
+    const cond = name ? { name: { $regex: name, $options: "i" } } : {};
+    let query = Product.find(cond);
+
+    // 페이지 값이 있는 경우 추가
+    if (page) {
+      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+      // 최종 total page
+      const totalItemNum = await Product.find(cond).countDocuments();
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+    }
+    // 검색조건 따로 실행
+    const productList = await query.exec();
+    res.status(200).json({ status: "success", data: productList });
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
